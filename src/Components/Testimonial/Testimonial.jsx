c218e0d67509ace3e2ff8c3877c99fa03d61e08c
Shubham Jain <shubham.rapariya2@gmail.com>
2023-12-08 01:29:06 +0530

    added data

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -8,6 +8,7 @@ import profilePic1 from '../../img/profile1.jpg'
 import profilePic2 from '../../img/profile2.jpg'
 import profilePic3 from '../../img/profile3.jpg'
 import profilePic4 from '../../img/profile4.jpg'
+import profilePic5 from '../../img/profile5.jpg'
 import { Element } from 'react-scroll'
 
 
@@ -18,27 +19,37 @@ const Testimonial = () => {
     const clients = [
         {
             img: profilePic1,
+            profile:"SDE @Microsoft",
             review:
-              "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+              "Outstanding job! The webpage exhibits excellent responsiveness, and the implementation of the scrollable product card section is spot on. The header is visually appealing, contributing to an overall great outcome. The design aligns perfectly with the specifications outlined in Figma, showcasing your precise and accurate implementation of every detail.",
           },
           {
             img: profilePic2,
+            profile:"Fronted Developer @Acciojob",
             review:
-              "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+              "Excellent work. Webpage is responsive, scrollable product card section is implemented properly. Header looks good. Overall work is great. Design is perfect wrt to figma .You have correctly implemented what you have been taught ---Instructor Ayush",
           },
           {
             img: profilePic3,
+            profile:"Software Developer @Walmart",
             review:
-              "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+              "Your promptness and the quality of your user interface have been outstanding. Your commitment and effectiveness are truly praiseworthy.",
           },
           {
             img: profilePic4,
+            profile:"Software Developer @Blue Yonder",
             review:
-              "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+              "You've been doing an excellent job with your responsiveness and the quality of your user interface. Your dedication and performance are truly commendable.",
+          },
+          {
+            img: profilePic5,
+            profile:"Frontend Developer @BlackBeltHelp",
+            review:
+              "Impressive work on both responsiveness and UI quality, showcasing outstanding commitment and performance. The project excels in adaptability, flawless execution of the product card section, and meticulous adherence to Figma specifications, resulting in an overall impressive outcome",
           },
     ]
 
-
+    
   return (
     <Element className="t-wrapper" name='testimonial'>
         <div className="t-heading">
@@ -68,6 +79,7 @@ const Testimonial = () => {
                         <SwiperSlide key={index}>
                             <div className="testimonial">
                             <img src={clients.img} alt="" />
+                            <p>{clients.profile}</p>
                             <p>{clients.review}</p>
                             </div>
                         </SwiperSlide>
