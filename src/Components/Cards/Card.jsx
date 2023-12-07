0a35cd650e19fe3b310bc53ee577b58b12f4d256
Shubham Jain <shubham.rapariya2@gmail.com>
2023-12-08 00:11:46 +0530

    bug fixes

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({emoji,heading,detail}) => {
       <img src={emoji} alt="" />
       <span>{heading}</span>
       <span>{detail}</span>
-      <button className="c-button">Learn More</button>
+      {/* <button className="c-button">Learn More</button> */}
     </div>
     
   )
