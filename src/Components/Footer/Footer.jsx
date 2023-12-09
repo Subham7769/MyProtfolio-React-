af35ce0d142593ad691eb27e899a0765565bfa9d
Shubham Jain <shubham.rapariya2@gmail.com>
2023-12-10 00:51:55 +0530

    final deployement

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Footer.css'
 import wave from '../../img/wave.png'
+import arrow from '../../img/Arrow.png'
 import Insta from '@iconscout/react-unicons/icons/uil-instagram'
 import Github from '@iconscout/react-unicons/icons/uil-github'
 import Linkedin from '@iconscout/react-unicons/icons/uil-linkedin'
 import HeartFilled  from '@iconscout/react-unicons/icons/uil-heart'
+import { Link } from 'react-scroll';
+
 
 const Footer = () => {
   return (
@@ -19,6 +22,8 @@ const Footer = () => {
         </div>
     <p>Made with  <HeartFilled  color='red' size='1rem'/>  By Subham Jain</p>
     </div>
+    
+    <span className='Arrow'><Link to="navbar" smooth={true} duration={300}><img src={arrow} alt="" /></Link></span>
     </div>
   )
 }
