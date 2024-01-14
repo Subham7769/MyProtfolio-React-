d1a108b6032d64e7cc5ecaf2f2b2886a2d9eac3d
Shubham Jain <shubham.rapariya2@gmail.com>
2024-01-14 19:22:52 +0530

    upgraded & added details

--- src/Components/Certifications/Certifications.jsx
+++ src/Components/Certifications/Certifications.jsx
@@ -1,28 +1,41 @@
 import React from 'react'
 import '../Portfolio/Portfolio.css'
-import {Swiper,SwiperSlide} from 'swiper/react';
+import {Swiper, SwiperSlide} from 'swiper/react';
 import 'swiper/css'
-import Atlassian from '../../Assets/Certificate/Agile with Atlassian Jira.png'
-import Meta_HTML from '../../Assets/Certificate/Meta_HTML & CSS in Depth 04.png'
-import Meta_Programmin from '../../Assets/Certificate/Meta_Programmin with javascript 04.png'
-import Meta_React_Basics from '../../Assets/Certificate/Meta_React_Basics 05.png'
-import Meta_Version from '../../Assets/Certificate/Meta_Version Control 03.png'
-import Meta_Introduction from '../../Assets/Certificate/Meta_Introduction to Front-End Development_01.jpeg'
-import Corporate_Telephone from '../../Assets/Certificate/Corporate Telephone Etiquette Batch 07.png'
-import Business_Atiquette from '../../Assets/Certificate/Business_Atiquette.png'
-import SoftSkills from '../../Assets/Certificate/SoftSkills.png'
-import Presentation from '../../Assets/Certificate/Presentation.png'
-import Improve_Interpersonal_Skills from '../../Assets/Certificate/Improve_Interpersonal_Skills.png'
-import Effective_Email_Writting from '../../Assets/Certificate/Effective_Email_Writting.png'
-import Prepare_a_Strong_Resume_and_Cover_Letter_Batch_06 from '../../Assets/Certificate/Prepare_a_Strong_Resume_and_Cover_Letter_Batch_06.png'
-import octanet from '../../Assets/Certificate/octanet Intership.png'
 import { Element } from 'react-scroll'
+import img1 from '../../Assets/Certificate/Agile with Atlassian Jira.png'
+import img2 from '../../Assets/Certificate/Meta_HTML & CSS in Depth 04.png'
+import img3 from '../../Assets/Certificate/Meta_Programmin with javascript 04.png'
+import img4 from '../../Assets/Certificate/Meta_React_Basics 05.png'
+import img5 from '../../Assets/Certificate/Meta_Version Control 03.png'
+import img6 from '../../Assets/Certificate/Meta_Introduction to Front-End Development_01.jpeg'
+import img7 from '../../Assets/Certificate/Corporate Telephone Etiquette Batch 07.png'
+import img8 from '../../Assets/Certificate/Business_Atiquette.png'
+import img9 from '../../Assets/Certificate/SoftSkills.png'
+import img10 from '../../Assets/Certificate/Presentation.png'
+import img11 from '../../Assets/Certificate/Improve_Interpersonal_Skills.png'
+import img12 from '../../Assets/Certificate/Effective_Email_Writting.png'
+import img13 from '../../Assets/Certificate/Prepare_a_Strong_Resume_and_Cover_Letter_Batch_06.png'
+import img14 from '../../Assets/Certificate/octanet Intership.png'
 
+const images = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14
+];
 
 const Certifications = () => {
-
-    
-    
   return (
     <Element className="portfolio" name="portfolio" style={{height:"70vh"}}>
         <span>Recent</span>
@@ -39,51 +52,14 @@ const Certifications = () => {
               slidesPerView: 3,
             }}}
         className='portfolio-slider'>
-            <SwiperSlide>
-                    <img src={Atlassian} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Meta_React_Basics} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Meta_Introduction} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Meta_Version} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Meta_Programmin} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Meta_HTML} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Corporate_Telephone} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Business_Atiquette} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={SoftSkills} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-               <img src={Presentation} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Improve_Interpersonal_Skills} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Effective_Email_Writting} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={octanet} alt="" />
-            </SwiperSlide>
+            {images.map((image, index) => (
+              <SwiperSlide key={index}>
+                <img src={image} alt="" />
+              </SwiperSlide>
+            ))}
         </Swiper>
     </Element>
   )
 }
 
 export default Certifications
-
-
-
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -2,25 +2,39 @@ import React from 'react'
 import './Portfolio.css'
 import {Swiper,SwiperSlide} from 'swiper/react';
 import 'swiper/css'
-import portfolio from '../../img/portfolio.jpeg'
-import youtube from '../../img/youtube.png'
-import AirBnb from '../../img/AirBnb.png'
-import cookpal from '../../img/cookpal.png'
-import blinkit from '../../img/blinkit.png'
-import acciojob from '../../img/acciojob.png'
-import Otarea from '../../img/Otarea.png'
-import food from '../../img/food.png'
-import Jira from '../../img/Jira.png'
-import Spotify from '../../img/Spotify.png'
-import youtube_react from '../../img/youtube_clone_react.png'
-import todoreact from '../../img/todoreact.png'
-import Imagebazzar from '../../img/Imagebazzar.png'
 import { Element } from 'react-scroll'
 
+import img1 from '../../img/youtube_clone_react.png'
+import img2 from '../../img/Imagebazzar.png'
+import img3 from '../../img/todoreact.png'
+import img4 from '../../img/portfolio.jpeg'
+import img5 from '../../img/blinkit.png'
+import img6 from '../../img/acciojob.png'
+import img7 from '../../img/Otarea.png'
+import img8 from '../../img/food.png'
+import img9 from '../../img/Jira.png'
+import img10 from '../../img/Spotify.png'
+import img11 from '../../img/cookpal.png'
+import img12 from '../../img/AirBnb.png'
+import img13 from '../../img/youtube.png'
+
+const images = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13
+];
 
 const Portfolio = () => {
-
-    
   return (
     <Element className="portfolio" name="portfolio" >
         <span>Recent Projects</span>
@@ -32,51 +46,14 @@ const Portfolio = () => {
         slidesPerView={1}
         grabCursor={true}
         className='portfolio-slider'>
-            <SwiperSlide>
-                <a target="_blank" href="https://youtubr-clone-react.vercel.app/"><img src={youtube_react} alt="" /><p className='live'>Live</p></a>
-            </SwiperSlide>
-            <SwiperSlide>
-                <a target="_blank" href="https://image-bazar-infinityscroll.vercel.app/"><img src={Imagebazzar} alt="" /><p className='live'>Live</p></a>
-            </SwiperSlide>
-            <SwiperSlide>
-                <a target="_blank" href="https://to-do-list-react-woad-zeta.vercel.app/"><img src={todoreact} alt="" /><p className='live'>Live</p></a>
-            </SwiperSlide>
-            <SwiperSlide>
-                <a target="_blank" href="https://my-protfolio-react-three.vercel.app/"><img src={portfolio} alt="" /><p className='live'>Live</p></a>
-            </SwiperSlide>
-            <SwiperSlide>
-                <a target="_blank" href="https://subham7769.github.io/Youtube-Clone/"><img src={youtube} alt="" /><p className='live'>Live</p></a>
-            </SwiperSlide>
-            <SwiperSlide>
-                <a target="_blank" href="https://subham7769.github.io/Airbnb-Clone/"><img src={AirBnb} alt="" /><p className='live'>Live</p></a>
-            </SwiperSlide>
-            <SwiperSlide>
-                <a target="_blank" href="https://subham7769.github.io/Cookpal_Menu_Catalogue/"><img src={cookpal} alt="" /><p className='live'>Live</p></a>
-            </SwiperSlide>
-            <SwiperSlide>
-                <a target="_blank" href="https://subham7769.github.io/Blinkit-clone/"><img src={blinkit} alt="" /><p className='live'>Live</p></a>
-            </SwiperSlide>
-            <SwiperSlide>
-                <a target="_blank" href="https://subham7769.github.io/Acciojob-New-Portal/"><img src={acciojob} alt="" /><p className='live'>Live</p></a>
-            </SwiperSlide>
-            <SwiperSlide>
-                <a target="_blank" href="https://subham7769.github.io/Otarea/"><img src={Otarea} alt="" /><p className='live'>Live</p></a>
-            </SwiperSlide>
-            <SwiperSlide>
-                <a target="_blank" href="https://subham7769.github.io/Food-delivery/"><img src={food} alt="" /><p className='live'>Live</p></a>
-            </SwiperSlide>
-            <SwiperSlide>
-                <a target="_blank" href="https://subham7769.github.io/Jira_clone/"><img src={Jira} alt="" /><p className='live'>Live</p></a>
-            </SwiperSlide>
-            <SwiperSlide>
-                <a target="_blank" href="https://subham7769.github.io/Spotify_clone/"><img src={Spotify} alt="" /><p className='live'>Live</p></a>
-            </SwiperSlide>
+            {images.map((image, index) => (
+              <SwiperSlide key={index}>
+                <a target="_blank" href="#"><img src={image} alt="" /><p className='live'>Live</p></a>
+              </SwiperSlide>
+            ))}
         </Swiper>
     </Element>
   )
 }
 
 export default Portfolio
-
-
-
