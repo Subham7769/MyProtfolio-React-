fbf8a5a8f4c92ea1c04f610ee9544b904e1c6e4f
Shubham Jain <shubham.rapariya2@gmail.com>
2024-01-14 20:32:58 +0530

    upgraded & added details

--- src/Components/Certifications/Certifications.jsx
+++ src/Components/Certifications/Certifications.jsx
@@ -3,20 +3,23 @@ import '../Portfolio/Portfolio.css'
 import {Swiper, SwiperSlide} from 'swiper/react';
 import 'swiper/css'
 import { Element } from 'react-scroll'
-import img1 from '../../Assets/Certificate/Agile with Atlassian Jira.png'
-import img2 from '../../Assets/Certificate/Meta_HTML & CSS in Depth 04.png'
-import img3 from '../../Assets/Certificate/Meta_Programmin with javascript 04.png'
-import img4 from '../../Assets/Certificate/Meta_React_Basics 05.png'
-import img5 from '../../Assets/Certificate/Meta_Version Control 03.png'
-import img6 from '../../Assets/Certificate/Meta_Introduction to Front-End Development_01.jpeg'
-import img7 from '../../Assets/Certificate/Corporate Telephone Etiquette Batch 07.png'
-import img8 from '../../Assets/Certificate/Business_Atiquette.png'
-import img9 from '../../Assets/Certificate/SoftSkills.png'
-import img10 from '../../Assets/Certificate/Presentation.png'
-import img11 from '../../Assets/Certificate/Improve_Interpersonal_Skills.png'
-import img12 from '../../Assets/Certificate/Effective_Email_Writting.png'
-import img13 from '../../Assets/Certificate/Prepare_a_Strong_Resume_and_Cover_Letter_Batch_06.png'
-import img14 from '../../Assets/Certificate/octanet Intership.png'
+
+import img1 from '../../Assets/Certificate/img (1).png'
+import img2 from '../../Assets/Certificate/img (2).png'
+import img3 from '../../Assets/Certificate/img (3).png'
+import img4 from '../../Assets/Certificate/img (4).png'
+import img5 from '../../Assets/Certificate/img (5).png'
+import img6 from '../../Assets/Certificate/img (6).png'
+import img7 from '../../Assets/Certificate/img (7).png'
+import img8 from '../../Assets/Certificate/img (8).png'
+import img9 from '../../Assets/Certificate/img (9).png'
+import img10 from '../../Assets/Certificate/img (10).png'
+import img11 from '../../Assets/Certificate/img (11).png'
+import img12 from '../../Assets/Certificate/img (12).png'
+import img13 from '../../Assets/Certificate/img (13).png'
+import img14 from '../../Assets/Certificate/img (14).png'
+import img15 from '../../Assets/Certificate/img (15).png'
+import img16 from '../../Assets/Certificate/img (16).png'
 
 const images = [
   img1,
@@ -32,7 +35,9 @@ const images = [
   img11,
   img12,
   img13,
-  img14
+  img14,
+  img15,
+  img16,
 ];
 
 const Certifications = () => {
